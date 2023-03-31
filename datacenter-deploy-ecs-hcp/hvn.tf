5b0efd8578406a707663339cc490856ca8caa048
tkaburagi <t.kaburagi@me.com>
2023-03-31 22:53:09 +0900

    prefix

--- datacenter-deploy-ecs-hcp/hvn.tf
+++ datacenter-deploy-ecs-hcp/hvn.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 resource "hcp_hvn" "server" {
-  hvn_id         = "main-hvn"
+  hvn_id         = "${var.name}-main-hvn"
   cloud_provider = "aws"
   region         = var.region
   cidr_block     = "172.25.16.0/20"
